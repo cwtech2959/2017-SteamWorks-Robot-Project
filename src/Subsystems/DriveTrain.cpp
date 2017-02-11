2f552f8413db06914ac4278d32c59665d557c75d
Cole Tubman <19rtubman@lmclakers.org>
2017-02-11 15:05:36 -0500

    Added CanTalonSRX Robot Drive

--- src/Subsystems/DriveTrain.cpp
+++ src/Subsystems/DriveTrain.cpp
@@ -1,8 +1,8 @@
 /*
  * DriveTrain.cpp
  *
- *  Created on: Jan 21, 2017
- *      Author: Kevin
+ *  Created on: Jan 28, 2017
+ *      Author: mriri
  */
 
 #include <Subsystems/DriveTrain.h>
@@ -11,27 +11,18 @@
 DriveTrain::DriveTrain() :
 		frc::Subsystem("DriveTrain")
 {
-	// TODO Auto-generated constructor stub
-
 }
 
 DriveTrain::~DriveTrain()
 {
-	// TODO Auto-generated destructor stub
 }
 
 void DriveTrain::InitDefaultCommand()
 {
-	// Set the default command for a subsystem here.
 	SetDefaultCommand(new DriveWithJoystick());
 }
 
-void DriveTrain::TankDrive(double leftSide, double rightSide)
-{
-	m_TankDrive.TankDrive(leftSide, rightSide, false);
-}
-
-void DriveTrain::TankDrive(frc::Joystick * pJoystick)
+void DriveTrain::TankDrive(frc::Joystick*pJoystick)
 {
 	TankDrive(pJoystick->GetRawAxis(1), pJoystick->GetRawAxis(3));
 }
@@ -39,6 +30,9 @@ void DriveTrain::TankDrive(frc::Joystick * pJoystick)
 void DriveTrain::Stop()
 {
 	TankDrive(0.0, 0.0);
-	//m_TankDrive.TankDrive(0.0, 0.0, false);
 }
 
+void DriveTrain::TankDrive(double leftSide, double rightSide)
+{
+	DriveTank.TankDrive(leftSide, rightSide);
+}
